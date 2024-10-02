d2f2015ed6c58990fa12d41d4b59091c936349ae
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-02 21:39:00 +0530

    initial tailwind and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <h1>Welcome to Streamify</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <h1 className='text-center bg-red-500 text-3xl'>Welcome to Streamify</h1>}/>
+      <Route path='/movie/:id' element={ <h1>Movie Detail Page</h1>}/>
+      <Route path='/movies/:type' element={ <h1>Movie Lists</h1>}/>
+      <Route path='/*' element={ <h1>Error 404 - PAGE NOT FOUND</h1>}/>
+   
+    </Routes>
+    </BrowserRouter>
   )
 }
 
