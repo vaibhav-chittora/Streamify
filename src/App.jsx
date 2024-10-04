dd155c3bc0786b83c6d8eebcb3f10de31c2fde84
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-04 22:32:13 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={ <h1 className='text-center bg-red-500 text-3xl'>Welcome to Streamify</h1>}/>
+      <Route path='/' element={ <Navbar/>}/>
       <Route path='/movie/:id' element={ <h1>Movie Detail Page</h1>}/>
       <Route path='/movies/:type' element={ <h1>Movie Lists</h1>}/>
       <Route path='/*' element={ <h1>Error 404 - PAGE NOT FOUND</h1>}/>
