2b4f71fd7455bb1dbd518402a582e6ff2e089080
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-09 20:57:51 +0530

    added carousel,made API call to trending movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/navbar/Navbar'
+import Home from './pages/Home'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={ <Navbar/>}/>
-      <Route path='/movie/:id' element={ <h1>Movie Detail Page</h1>}/>
-      <Route path='/movies/:type' element={ <h1>Movie Lists</h1>}/>
-      <Route path='/*' element={ <h1>Error 404 - PAGE NOT FOUND</h1>}/>
-   
-    </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:id' element={<h1>Movie Detail Page</h1>} />
+          <Route path='/movies/:type' element={<h1>Movie Lists</h1>} />
+          <Route path='/*' element={<h1>Error 404 - PAGE NOT FOUND</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
