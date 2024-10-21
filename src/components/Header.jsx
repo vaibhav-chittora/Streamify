645c7f59f76be592e85c2691976df570fe6b9747
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-10-21 23:38:25 +0530

    quick fix-navbar:more option feature on small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import logo from '../assets/logo1.png'
 import avatar from '../assets/avatar.png'
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from 'react-icons/hi2';
-import { HiPlus, HiDotsVertical  } from 'react-icons/hi';
+import { HiPlus, HiDotsVertical } from 'react-icons/hi';
 import HeaderItem from './HeaderItem';
 
 
 function Header() {
+  const [show, setShow] = React.useState(false)
+
   const menu = [
     {
       name: 'Home',
@@ -49,12 +51,17 @@ function Header() {
 
         <div className='flex md:hidden gap-14'>
           <img src={logo} className='w-10 md:w-16 object-cover' alt='logo' />
-          {menu.map((item, index) =>index<3 && (
-            <HeaderItem name={item.name} Icon={item.icon} key={index} />
+          {menu.map((item, index) => index < 3 && (
+            <HeaderItem name={''} Icon={item.icon} key={index} />
           ))}
         </div>
-        <div className='md:hidden'>
-          <HeaderItem name='More' Icon={HiDotsVertical} />
+        <div className='md:hidden' onClick={() => setShow(!show)}>
+          <HeaderItem name='' Icon={HiDotsVertical} />
+          {show ? <div className='absolute mt-3 bg-[#181818] border border-gray-500 px-5 py-4'>
+            {menu.map((item, index) => index > 2 && (
+              <HeaderItem name={item.name} Icon={item.icon} key={index} />
+            ))}
+          </div> : null}
 
         </div>
 
